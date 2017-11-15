50a203d04ba443d3b47edfc11e4cf9eb01f74e99
wikke <wikkefly@gmail.com>
2017-11-15 12:16:33 +0800

    add BatchNormalization in VGG

--- classify_model.py
+++ classify_model.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, Flatten
+from keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, Flatten, BatchNormalization
 from keras.optimizers import Adam
 from config import *
 
@@ -13,14 +13,23 @@ def get_simplified_VGG_classifier():
     x = Conv3D(16, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
 
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
+
     x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
 
     x = Conv3D(64, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(64, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(64, (3, 3, 3), padding='same', activation='relu')(x)
     # x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
 
     # x = Conv3D(256, (3, 3, 3), padding='same', activation='relu')(x)
     # x = Conv3D(256, (3, 3, 3), padding='same', activation='relu')(x)
@@ -44,24 +53,38 @@ def get_simplified_VGG_classifier():
 
 def get_full_VGG_classifier():
     inputs = Input((CLASSIFY_INPUT_WIDTH, CLASSIFY_INPUT_HEIGHT, CLASSIFY_INPUT_DEPTH, CLASSIFY_INPUT_CHANNEL))
+    x = inputs
 
-    x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(inputs)
+
+    x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
 
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
+
     x = Conv3D(64, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(64, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
 
     x = Conv3D(128, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(128, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(128, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
 
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
+
     x = Conv3D(256, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(256, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(256, (3, 3, 3), padding='same', activation='relu')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
 
     x = Conv3D(512, (3, 3, 3), padding='same', activation='relu')(x)
     x = Conv3D(512, (3, 3, 3), padding='same', activation='relu')(x)

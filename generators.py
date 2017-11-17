37a4db859aec889bb7c64dc84312ef671a3b3030
wikke <wikkefly@gmail.com>
2017-11-17 12:56:24 +0800

    tons of data augumentations

--- generators.py
+++ generators.py
@@ -54,13 +54,10 @@ def get_tumor_records():
     records.dropna(inplace=True)
 
     print('tumor record size {}'.format(records.shape))
-    if DEBUG_ONLY_TRAIN_SWITCHER_ON:
+    if DEBUG_ONLY_TRAIN_FINE_CUT_BIG_TUMOR_SWITCHER:
         records.drop(records[records.cover_ratio < DEBUG_ONLY_TRAIN_COVER_RATIO_BIGGER_THAN].index, axis=0, inplace=True)
         records.drop(records[records.diameter_mm < DEBUG_ONLY_TRAIN_TUMOR_DIAMETER_LARGER_THAN].index, axis=0, inplace=True)
         print('after drop, tumor record size {}'.format(records.shape))
-
-    if RANDOMIZE_RECORDS:
-        records = records.iloc[np.random.permutation(records.shape[0])]
 
     return records
 
@@ -69,9 +66,31 @@ tumor_records_len = tumor_records.shape[0]
 if tumor_records_len == 0:
     print('no tumor records, generator cannot work')
     exit()
+
 tumor_records_train = tumor_records[:int(tumor_records_len * TRAIN_VAL_RATIO)]
 tumor_records_val = tumor_records[int(tumor_records_len * TRAIN_VAL_RATIO):]
 del tumor_records, tumor_records_len
+
+re_sample = (TRAIN_CLASSIFY_NOT_SEGMENTATION and TRAIN_CLASSIFY_ENABLE_DATA_AUGUMENTATION) \
+            or ((not TRAIN_CLASSIFY_NOT_SEGMENTATION) and TRAIN_SEG_ENABLE_DATA_AUGUMENTATION)
+if re_sample:
+    r_10 = tumor_records_train[tumor_records_train.diameter_mm < 10.0]
+    r_30 = tumor_records_train[(tumor_records_train.diameter_mm >= 10.0) & (tumor_records_train.diameter_mm < 30.0)]
+    r_more = tumor_records_train[tumor_records_train.diameter_mm >= 30.0]
+
+    concats = []
+    for _ in range(RESAMPLE_DATA_LESS_10_RATIO):
+        concats.append(r_10)
+    for _ in range(RESAMPLE_DATA_LESS_30_RATIO):
+        concats.append(r_30)
+    concats.append(r_more)
+    tumor_records_train = pd.concat(concats, axis=0)
+
+    print('after resample, got {} samples'.format(tumor_records_train.shape[0]))
+
+if RANDOMIZE_RECORDS:
+    tumor_records_train = tumor_records_train.iloc[np.random.permutation(tumor_records_train.shape[0])]
+    print('tumor_records_train randomized')
 
 # random_offset works iff around_tumor=True
 def get_block(record, around_tumor=True, random_offset=(0, 0, 0), shape=(INPUT_WIDTH, INPUT_HEIGHT, INPUT_DEPTH)):
@@ -93,6 +112,10 @@ def get_block(record, around_tumor=True, random_offset=(0, 0, 0), shape=(INPUT_W
         block[block==0] = np.min(hf['img'])
         block = (block - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
         block = np.clip(block, 0.0, 1.0)
+
+        # DenseNet paper suggests to (img-mean)/std, use this for simple
+        if TRAIN_CLASSIFY_NOT_SEGMENTATION and TRAIN_CLASSIFY_MODEL.lower() == 'densenet':
+            block = block - 0.5
 
         return block
 
@@ -117,18 +140,26 @@ def get_seg_batch(batch_size=32, from_train=True, random_choice=False):
             record = records.iloc[idx]
 
             is_positive_sample = random.random() < TRAIN_SEG_POSITIVE_SAMPLE_RATIO
-            random_offset = np.array([
-                random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET),
-                random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET),
-                random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET)
-            ])
+            random_offset = np.array([0, 0, 0])
+            if TRAIN_SEG_ENABLE_DATA_AUGUMENTATION:
+                random_offset = np.array([
+                    random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET),
+                    random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET),
+                    random.randrange(-TRAIN_SEG_SAMPLE_RANDOM_OFFSET, TRAIN_SEG_SAMPLE_RANDOM_OFFSET)
+                ])
 
             X[b,:,:,:,0] = get_block(record, around_tumor=is_positive_sample, random_offset=random_offset,
                                      shape=(INPUT_WIDTH, INPUT_HEIGHT, INPUT_DEPTH))
             y[b,:,:,:,0] = make_seg_mask(record, create_mask=is_positive_sample, random_offset=random_offset)
 
-            if not DEBUG_SEG_TRY_OVERFIT:
-                idx = idx + 1 if idx < records.shape[0] - 1 else 0
+            idx = idx + 1 if idx < records.shape[0] - 1 else 0
+
+        # rotate
+        if TRAIN_SEG_ENABLE_DATA_AUGUMENTATION:
+            for b in range(batch_size):
+                _perm = np.random.permutation(3)
+                X[b, :, :, :, 0] = np.transpose(X[b, :, :, :, 0], _perm)
+                y[b, :, :, :, 0] = np.transpose(y[b, :, :, :, 0], _perm)
 
         if DEBUG_PLOT_WHEN_GETTING_SEG_BATCH:
             plot_batch_sample(X, y)
@@ -155,14 +186,14 @@ def make_seg_mask(record, create_mask=True, random_offset=(0, 0, 0)):
 
     return mask
 
-# [1, 0], positive sample
-# [0, 1], negative sample
+# 1:3比例
+
 def get_classify_batch(batch_size=32, from_train=True, random_choice=False):
     idx = 0
     records = tumor_records_train if from_train else tumor_records_val
 
     shape = (CLASSIFY_INPUT_WIDTH, CLASSIFY_INPUT_HEIGHT, CLASSIFY_INPUT_DEPTH)
-    positive_num = int(batch_size * CLASSIFY_POSITIVE_SAMPLE_RATIO)
+    positive_num = int(batch_size * TRAIN_CLASSIFY_POSITIVE_SAMPLE_RATIO)
     X = np.zeros((batch_size, CLASSIFY_INPUT_WIDTH, CLASSIFY_INPUT_HEIGHT, CLASSIFY_INPUT_DEPTH, CLASSIFY_INPUT_CHANNEL))
     y = np.zeros((batch_size, 2))
 
@@ -172,11 +203,13 @@ def get_classify_batch(batch_size=32, from_train=True, random_choice=False):
                 idx = random.randint(0, records.shape[0] - 1)
             record = records.iloc[idx]
 
-            random_offset = np.array([
-                random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET),
-                random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET),
-                random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET)
-            ])
+            random_offset = np.array([0, 0, 0])
+            if TRAIN_CLASSIFY_ENABLE_DATA_AUGUMENTATION:
+                random_offset = np.array([
+                    random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET),
+                    random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET),
+                    random.randrange(-TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET, TRAIN_CLASSIFY_SAMPLE_RANDOM_OFFSET)
+                ])
 
             X[b, :, :, :, 0] = get_block(record, around_tumor=True, random_offset=random_offset, shape=shape)
             y[b, 0] = 1
@@ -187,5 +220,10 @@ def get_classify_batch(batch_size=32, from_train=True, random_choice=False):
             record = records.iloc[random.randint(0, records.shape[0] - 1)]
             X[b, :, :, :, 0] = get_block(record, around_tumor=False, shape=shape)
             y[b, 1] = 1
+
+        # rotate
+        if TRAIN_CLASSIFY_ENABLE_DATA_AUGUMENTATION:
+            for b in range(batch_size):
+                X[b, :, :, :, 0] = np.transpose(X[b, :, :, :, 0], np.random.permutation(3))
 
         yield X, y
